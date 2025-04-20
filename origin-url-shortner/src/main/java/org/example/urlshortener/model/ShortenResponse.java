4867eeb627572b8aa795a6bff41851775bf518f6
Sunnyday16 <sunny.k.kr@gmail.com>
2025-04-20 17:07:00 +1000

    Delete origin-url-shortner directory

--- origin-url-shortner/src/main/java/org/example/urlshortener/model/ShortenResponse.java
+++
@@ -1,3 +0,0 @@
-package org.example.urlshortener.model;
-
-public record ShortenResponse(String shortUrl) { }
--- origin-url-shortner/src/main/java/org/example/urlshortener/model/UrlMappingResponse.java
+++
@@ -1,9 +0,0 @@
-package org.example.urlshortener.model;
-
-import java.time.LocalDateTime;
-
-public record UrlMappingResponse(
-    String originalUrl,
-    String shortUrl,
-    LocalDateTime createdAt
-) {}
